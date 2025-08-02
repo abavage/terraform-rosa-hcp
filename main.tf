9ce8684ea796c8127ec5c1ad9c75a23b7c24cd76
Andrew Bavage <abavage@gmail.com>
2025-08-02 14:13:33 +1000

    changes

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
-#terraform {
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = ">= 4.20.0"
-#    }
-#    rhcs = {
-#      version = "= 1.6.8"
-#      source  = "terraform-redhat/rhcs"
-#    }
-#  }
-#}
-
 # Export token using the RHCS_TOKEN environment variable
+# export RHCS_TOKEN="..."
+
 provider "rhcs" {}
 
 provider "aws" {
@@ -23,35 +12,6 @@ provider "aws" {
     tags = local.tags
   }
 }
-
-
-
-#data "aws_availability_zones" "available" {}
-
-#locals {
-  # Extract availability zone names for the specified region, limit it to 3 if multi az or 1 if single
-#  region_azs = var.multi_az ? slice([for zone in data.aws_availability_zones.available.names : format("%s", zone)], 0, 3) : slice([for zone in data.aws_availability_zones.available.names : format("%s", zone)], 0, 1)
-#}
-
-#resource "random_string" "random_name" {
-#  length  = 6
-#  special = false
-#  upper   = false
-#}
-
-#locals {
-#  worker_node_replicas = var.multi_az ? 3 : 2
-  # If cluster_name is not null, use that, otherwise generate a random cluster name
-  #cluster_name = coalesce(var.cluster_name, "rosa-\${random_string.random_name.result}")
-#  cluster_name = var.cluster_name
-#}
-
-# The network validator requires an additional 60 seconds to validate Terraform clusters.
-#resource "time_sleep" "wait_60_seconds" {
-#  count = var.create_vpc ? 1 : 0
-#  depends_on = [module.vpc]
-#  create_duration = "60s"
-#}
 
 module "rosa-hcp" {
   source                 = "terraform-redhat/rosa-hcp/rhcs"
@@ -174,43 +134,3 @@ resource "random_string" "random" {
   min_lower   = 4
   min_upper   = 4
 }
-
-
-
-#module "rhcs_hcp_machine_pool" {
-  #source   = "git::https://github.com/terraform-redhat/terraform-rhcs-rosa-hcp.git//modules/machine-pool"
-#  source   = "terraform-redhat/rosa-hcp/rhcs//modules/machine-pool"
-#  for_each = var.machine_pools
-
-#  cluster_id                   = try(module.rosa-hcp.cluster_id, "dummy")
-#  name                         = each.value.name
-#  auto_repair                  = try(each.value.auto_repair, null)
-#  autoscaling                  = try(each.value.autoscaling, null)
-#  aws_node_pool                = each.value.aws_node_pool
-#  openshift_version            = try(each.value.openshift_version, null)
-#  tuning_configs               = try(each.value.tuning_configs, null)
-#  upgrade_acknowledgements_for = try(each.value.upgrade_acknowledgements_for, null)
-#  replicas                     = try(each.value.replicas, null)
-#  taints                       = try(each.value.taints, null)
-#  labels                       = try(each.value.labels, null)
-#  subnet_id                    = each.value.subnet_id
-#  kubelet_configs              = try(each.value.kubelet_configs, null)
-#  ignore_deletion_error        = try(each.value.ignore_deletion_error, false)
-#}
-
-
-
-#module "htpasswd_idp" {
-#  source = "terraform-redhat/rosa-hcp/rhcs//modules/idp"
-#
-#  cluster_id         = module.rosa-hcp.cluster_id
-#  name               = "htpasswd-idp"
-#  idp_type           = "htpasswd"
-#  htpasswd_idp_users = jsonencode( 
-#    [
-#      { username = "some-user", 
-#        password = "Some-Complicated-123-Password"
-#      }
-#    ]
-#  )
-#}
